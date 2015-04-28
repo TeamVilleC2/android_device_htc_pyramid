6f4e1f8ee4381513744aa93bdcc51a35cf378563
McNulty Gracie <mg@ecake.in>
2015-04-28 13:21:32 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,167 +20,162 @@
 # definition file).
 #
 
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-
-LOCAL_PATH := device/htc/pyramid
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
 BOARD_VENDOR := htc
-
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := villec2
+BOARD_WANTS_EMMC_BOOT := true
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_LEGACY_LPA := true
+LOCAL_CFLAGS += LEGACY_LPA
+# Kernel
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
+BOARD_KERNEL_PAGE_SIZE := 2048
+TARGET_KERNEL_CONFIG := villec2_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/villec2
+TARGET_SPECIFIC_HEADER_PATH := device/htc/villec2/include
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=villec2 no_console_suspend=1 androidboot.selinux=permissive
 
-# Platform
+# Sensors
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Board
 TARGET_BOARD_PLATFORM := msm8660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_NEEDS_NON_PIE_SUPPORT := true
-
 # Flags
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DHTCLOG
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
+# we are using prebuilt in vendor tree
+TARGET_QCOM_DISPLAY_VARIANT := false
+TARGET_QCOM_MEDIA_VARIANT := villec2
+
+
+# FB legacy
+BOARD_EGL_NEEDS_LEGACY_FB := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-
-# Audio
 COMMON_GLOBAL_CFLAGS += -DHTC_ACOUSTIC_AUDIO -DLEGACY_QCOM_VOICE
+BOARD_USE_QCOM_LPA := true
+TARGET_QCOM_AUDIO_VARIANT := caf
 BOARD_USES_LEGACY_ALSA_AUDIO := true
+BOARD_QCOM_TUNNEL_LPA_ENABLED := false
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+# TARGET_NEW_ION_API := true
 
-# Camera
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-USE_DEVICE_SPECIFIC_CAMERA := true
-BOARD_CAMERA_FRONT_VGA := true
 
-# FM Radio
-# 4.2 -> commenting these for audio policy
-#BOARD_HAVE_QCOM_FM := true
-#COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED
+# Bluetooth
 
-# GPS
-TARGET_GPS_HAL_PATH := $(LOCAL_PATH)/gps
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
-
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/villec2/bluetooth/vnd_msm8660.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/villec2/bluetooth/include
 
 # Graphics
 USE_OPENGL_RENDERER := true
-TARGET_DISPLAY_USE_RETIRE_FENCE := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-BOARD_EGL_CFG := device/htc/pyramid/configs/egl.cfg
-TARGET_DISPLAY_INSECURE_MM_HEAP := true
+BOARD_EGL_CFG := device/htc/villec2/configs/egl.cfg
+# Filesystem
+BOARD_VOLD_MAX_PARTITIONS := 36
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
 
-# Media
-TARGET_NO_ADAPTIVE_PLAYBACK := true
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
-# Use CM PowerHAL
-TARGET_POWERHAL_VARIANT := cm
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := villec2
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
-
-# Webkit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := pyramid
-
-# Kernel
-BOARD_KERNEL_BASE := 0x48000000
-BOARD_KERNEL_PAGE_SIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=pyramid no_console_suspend=1 androidboot.selinux=permissive
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
-TARGET_KERNEL_CONFIG := pyramid_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/pyramid
-
-TARGET_RECOVERY_FSTAB := device/htc/pyramid/rootdir/etc/fstab.pyramid
-RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_DEVICE_DIRS += device/htc/pyramid
-
-# Required for Sensors
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-# Charge mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
-
-# Lights
-TARGET_HAS_NO_BLUE_LED := true
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/pyramid/bluetooth/include
-BOARD_BLUEDROID_VENDOR_CONF := device/htc/pyramid/bluetooth/vnd_pyramid.txt
-BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
+TARGET_PROVIDES_LIBLIGHTS := true
 
 # Bluetooth/Wifi
--include device/htc/pyramid/bcmdhd.mk
-
-# Hardware tunables
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
-
-# RIL
-BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
-
-# Misc
-BOARD_USES_LEGACY_MMAP := true
-
-# Use dlmalloc
-MALLOC_IMPL := dlmalloc
-
-# Recovery
-TARGET_RECOVERY_DEVICE_MODULES += chargeled
-
-# SELinux
--include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/htc/pyramid/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    platform_app.te \
-    property_contexts \
-    healthd.te \
-    recovery.te
+-include device/htc/villec2/bcmdhd.mk
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_VOLD_MAX_PARTITIONS := 36
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838859776
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1252770816
 BOARD_FLASH_BLOCK_SIZE := 262144
 
-# Twrp
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/htc/villec2/ramdisk/fstab.villec2
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+
+# Camera
+USE_CAMERA_STUB := false
+TARGET_PROVIDES_CAMERA_HAL := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
+COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838859776
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1252770816
+BOARD_FLASH_BLOCK_SIZE := 262144
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := false
+RECOVERY_FSTAB_VERSION := 2
+# Audio config
+
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+# Use libril in the device tree
+BOARD_USES_LEGACY_RIL := true
+
+# Graphics
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DREFRESH_RATE=60 -DHTC_RGBA_8888_OFFSET
+USE_OPENGL_RENDERER := true
+TARGET_NO_HW_VSYNC := true
+TARGET_USES_C2D_COMPOSITION := true
+SKIP_SET_METADATA := true
+
+
+# Misc
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
+
+# TWRP
 DEVICE_RESOLUTION = 540x960
-BOARD_HAS_NO_REAL_SDCARD := false
 TW_FLASH_FROM_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
 TW_INCLUDE_CRYPTO := true
-TW_NO_SCREEN_BLANK := true
-TW_INCLUDE_DUMLOCK := true
-HAVE_SELINUX := true
-TW_NO_CPU_TEMP := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
